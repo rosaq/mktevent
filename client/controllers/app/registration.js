565ff23fb689f08e0e8e1bb52fe50c05142c8433
Rosa Quintero <rosa.quintero@flex.com>
2017-09-07 13:09:14 -0700

    [Studio] Modified screen "registration"

--- client/controllers/app/registration.js
+++ client/controllers/app/registration.js
@@ -2,4 +2,9 @@ angular.module('app').controller('app_registration', app_registration);
 function app_registration($scope, app) {
     'use strict';
     app.init($scope);
+    
+    //Send base64 string to server
+    $scope.speakingChange = function () {
+        alert("Hi speakear");
+    }
 }
